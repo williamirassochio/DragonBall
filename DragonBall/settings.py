314f82adc7ce375c40cd7f89e945e4cd7ce8217f
William Irassochio <williamarroiodosal@gmail.com>
2022-10-10 17:22:18 -0300

    Modificações na base.html e barra de navegação navbar.html

--- DragonBall/settings.py
+++ DragonBall/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DragonBall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,11 +119,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static"
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media",
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
