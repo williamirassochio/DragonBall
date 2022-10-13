992d0c43d9cd3dd6cbfb8a2f14f1d1f4f2add0cc
William Irassochio <williamarroiodosal@gmail.com>
2022-10-13 16:55:14 -0300

    Trabalhando linkando os href nas urls

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import HomeFilmes, Homepage, Detalhesfilme
 
+app_name = 'filme'
+
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', HomeFilmes.as_view()),
-    path('filmes/<int:pk>', Detalhesfilme.as_view()),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', HomeFilmes.as_view(), name="homefilmes"),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilmes'),
 ]
