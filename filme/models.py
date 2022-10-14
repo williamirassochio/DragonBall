a703a2a6be36b0dcd4b6a042a6bd01f242a952d6
William Irassochio <williamarroiodosal@gmail.com>
2022-10-13 23:27:40 -0300

    Integrando link para filmes(episodios)

--- filme/models.py
+++ filme/models.py
@@ -12,3 +12,11 @@ class Filme(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Episodio(models.Model):
+    filme = models.ForeignKey("Filme", related_name="episodios", on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    video = models.URLField()
+
+    def __str__(self):
+     return self.filme.titulo +' - '+ self.titulo
