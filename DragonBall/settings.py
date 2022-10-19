947f5d0ee3423e576bd0376575be37274cd05f2c
William Irassochio <williamarroiodosal@gmail.com>
2022-10-19 08:30:15 -0300

    Adicionando campo ,filmes vistos, no DB

--- DragonBall/settings.py
+++ DragonBall/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = "filme.Usuario"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- filme/models.py
+++ filme/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -20,3 +21,6 @@ class Episodio(models.Model):
 
     def __str__(self):
      return self.filme.titulo +' - '+ self.titulo
+
+class Usuario(AbstractUser):
+    filmes_vistos = models.ManyToManyField("Filme")
